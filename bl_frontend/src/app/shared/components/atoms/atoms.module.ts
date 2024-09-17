18ecd9b0167d18356cd91ff6173d74b84767e9b7
LimpidCrypto <konermann@limpidcrypto.com>
2024-09-17 08:03:49 +0000

    add frontend

---
+++ bl_frontend/src/app/shared/components/atoms/atoms.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackgroundImageComponent } from './background-image/background-image.component';
+import { ButtonComponent } from './button/button.component';
+import { CircleComponent } from './circle/circle.component';
+import { HeroImageComponent } from './hero-image/hero-image.component';
+import { ImageComponent } from './image/image.component';
+import { LinkComponent } from './link/link.component';
+import { ModalComponent } from './modal/modal.component';
+import { Rotate180Component } from './rotate180/rotate180.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+
+@NgModule({
+  declarations: [
+    BackgroundImageComponent,
+    ButtonComponent,
+    CircleComponent,
+    HeroImageComponent,
+    ImageComponent,
+    LinkComponent,
+    ModalComponent,
+    Rotate180Component,
+    NavigationComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BackgroundImageComponent,
+    ButtonComponent,
+    CircleComponent,
+    HeroImageComponent,
+    ImageComponent,
+    LinkComponent,
+    ModalComponent,
+    Rotate180Component,
+    NavigationComponent,
+  ]
+})
+export class AtomsModule { }
